0cef57b604d3518afe49975b95bbc45e8d8dbfc4
Alexei <savca.alexei@gmail.com>
2026-09-06 18:47:55 +0300

    more test cases

--- test_myreduce.py
+++ test_myreduce.py
@@ -1,13 +1,16 @@
 import pytest
 from myreduce import myreduce
 from random import randint
+from functools import reduce
 
 def sum(a, b):
     return a + b
 
+
 def test_empty_list_with_no_initial():
     with pytest.raises(ValueError, match="empty list with no initial value"):
         myreduce(lambda x, y: x + y, [])
+
 
 def test_empty_list_with_initial():
     # input = [random.randint(1, 100) for _ in range(5)]
@@ -17,6 +20,7 @@ def test_empty_list_with_initial():
 
     assert result == initial
 
+
 def test_list_with_one_element():
     rand = randint(1, 100)
     input = [rand]
@@ -24,3 +28,22 @@ def test_list_with_one_element():
     result = myreduce(sum, input)
 
     assert result == rand
+
+
+def test_nontrivial_list():
+    input = [randint(1, 100) for _ in range(5)]
+    expected = reduce(sum, input)
+
+    result = myreduce(sum, input)
+
+    assert result == expected
+
+
+def test_nontrivial_with_initial():
+    input = [randint(1, 100) for _ in range(5)]
+    initial = randint(1, 1000)
+    expected = reduce(sum, input, initial)
+
+    result = myreduce(sum, input, initial)
+
+    assert result == expected
