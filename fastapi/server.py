fd031d5182f1974104a7436f1b006eeba6becb4b
Alexei <savca.alexei@gmail.com>
2026-09-14 22:40:44 +0300

    added /register and GET /users

--- fastapi/server.py
+++ fastapi/server.py
@@ -1,17 +1,55 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Depends, HTTPException
+from sqlmodel import create_engine, SQLModel, Field, select, Session
+import bcrypt
+from typing import List
+from sqlalchemy.orm import load_only
 
 app = FastAPI()
 
-@app.get("/")
-def get_root():
-    return {"status": "online"}
+engine = create_engine("sqlite:///database.db", connect_args={"check_same_thread": False})
 
-@app.get("/users")
-def get_users():
-    return {"users": []}
+def hash(pwd: str) -> str:
+    pwd_bytes = pwd.encode('utf-8')
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(pwd_bytes, salt).decode('utf-8')
 
-@app.get("/users/{id}")
-def get_user(id):
-    return {"user": {
-        "user_id": id
-    }}
+def get_db():
+    with Session(engine) as session:
+        yield session
+
+class UserBase(SQLModel):
+    email: str = Field(primary_key=True, index=True)
+
+class UserRegister(UserBase):
+    password: str
+
+class User(UserBase, table=True):
+    __tablename__ = "user"
+    password: str
+
+SQLModel.metadata.create_all(engine)
+
+@app.get("/users", response_model=List[UserBase], status_code=status.HTTP_200_OK)
+def get_users(db: Session = Depends(get_db)):
+    statement = select(User).options(load_only(User.email))
+    users = db.exec(statement).all()
+    return users
+
+@app.post("/register", status_code=status.HTTP_201_CREATED)
+def register_user(user_data: UserRegister, db: Session = Depends(get_db)):
+    statement = select(User).where(User.email == user_data.email)
+    existing_user = db.exec(statement).first()
+    print(user_data.password)
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Username exists"
+        )
+
+    new_user = User(
+        email=user_data.email,
+        password=hash(user_data.password)
+    )
+
+    db.add(new_user)
+    db.commit()
